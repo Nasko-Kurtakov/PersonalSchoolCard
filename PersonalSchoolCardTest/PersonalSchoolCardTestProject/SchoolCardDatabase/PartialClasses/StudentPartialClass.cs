8e824cbfa259785ca3e5dc8a67d767a1dccd9e35
Nasko-Kurtakov <nasko.kurtakov@dir.bg>
2015-01-20 21:39:06 +0200

    Added the test project of the database

---
+++ PersonalSchoolCardTest/PersonalSchoolCardTestProject/SchoolCardDatabase/PartialClasses/StudentPartialClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCardDatabase
+{
+    public partial class Student
+    {
+        public override string ToString()
+        {
+            return string.Format("Name: {0} Second Name: {1} Last Name: {2} PersonalID: {3}", this.FirstName, this.SecondName, this.LastName, this.PersonalNumber);
+        }
+    }
+}
