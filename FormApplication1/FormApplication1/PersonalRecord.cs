9f48a703327d04be67023c16b80e097b6fb43dc7
Alex Goranov <aleksgoranov155@gmail.com>
2015-01-21 09:13:50 -0800

    Form &&
    No database here

---
+++ FormApplication1/FormApplication1/PersonalRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormApplication1
+{
+    public partial class PersonalRecord : Form
+    {
+        public PersonalRecord()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            Success sc = new Success();
+            sc.Show();
+        }
+
+    }
+}
