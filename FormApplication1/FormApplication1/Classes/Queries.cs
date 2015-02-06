e16647665b394de957d0e701729d4d416d181909
Nasko-Kurtakov <nasko.kurtakov@dir.bg>
2015-02-06 15:25:28 +0200

    Try commiting

---
+++ FormApplication1/FormApplication1/Classes/Queries.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalSchoolCard.Data;
+
+namespace PersonalShcoolCard.Classes
+{
+    class Queries
+    {
+        public static string GetStudentsName(int studentID)
+        {
+            try
+            {
+                using (var context = new PersonalSchoolCardEntities())
+                {
+                    var sn = context.Students
+                        .Where(student => student.StudentID == studentID)
+                        .Select(student => new
+                        {
+                            FirstName = student.FirstName,
+                            SecondName = student.SecondName,
+                            LastName = student.LastName
+                        })
+                        .FirstOrDefault()
+                        ;
+
+                    var nameS
+                        = sn.FirstName + " " + sn.SecondName + " " + sn.LastName;
+
+                    return nameS;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static string GetStudentArea(int studentID)
+        {
+            using (var context = new PersonalSchoolCardEntities())
+            {
+                var AreaName = (from s in context.Students
+                                join set in context.Settlements
+                                on s.SettlementID equals set.SettlementID
+                                join ar in context.Areas
+                                on set.AreaID equals ar.AreaID
+                                where s.StudentID == studentID
+                                select ar.AreaName)
+                                .FirstOrDefault()
+                                ;
+
+                return AreaName;
+            }
+        }
+
+        public static string GetStudentsSettlement(int studentID)
+        {
+            using (var context = new PersonalSchoolCardEntities())
+            {
+                var StudentSettlement = context.Students
+                     .Where(student => student.StudentID == studentID)
+                    .Join(context.Settlements,
+                    o => o.SettlementID, od => od.SettlementID,
+                    (o, od) => new
+                    {
+                        Settlement = od.SettlemnetName
+                    })
+                    .FirstOrDefault()
+                    ;
+
+                return StudentSettlement.Settlement;
+            }
+        }
+
+        public static string GetStudentMunicipality(int studentID)
+        {
+            using (var context = new PersonalSchoolCardEntities())
+            {
+                var StudentMunicipality = (from s in context.Students
+                                           join set in context.Settlements
+                                           on s.SettlementID equals set.SettlementID
+                                           join mu in context.Manicipalities
+                                           on set.ManicipalityID equals mu.ManicipalityID
+                                           where s.StudentID == studentID
+                                           select mu.ManicipalityName)
+                                           .FirstOrDefault()
+                                           ;
+
+                return StudentMunicipality;
+            }
+        }
+
+        public static string GetStudentSchoolName()
+        {
+            using (var context = new PersonalSchoolCardEntities())
+            {
+                var SchoolName = (from s in context.Schools
+                                  select s.SchoolName)
+                                  .FirstOrDefault()
+                                  ;
+
+                return SchoolName;
+            }
+        }
+
+        public static string GetStudentID(int studentID)
+        {
+            using (var context = new PersonalSchoolCardEntities())
+            {
+                var StudentID = context.Students
+                    .Where(student => student.StudentID == studentID)
+                    .Select(student => student.PersonalNumber)
+                    .FirstOrDefault()
+                    ;
+
+                return StudentID;
+            }
+        }
+
+        public static string GetStudentCardID(int studentID)
+        {
+            using (var context = new PersonalSchoolCardEntities())
+            {
+
+                var StudentCardID = context.Students
+                    .Where(student => student.StudentID == studentID)
+                    .Select(student => student.PersonalCardNumber)
+                    .FirstOrDefault()
+                    ;
+
+                return StudentCardID;
+
+            }
+        }
+
+        public static string GetStudentCitizenship()
+        {
+            //using ( var context = new PersonalSchoolCEntities())
+            //{
+            //  code
+            //  code
+            //  code
+            //  return code;
+            //}
+
+            return "българско";
+
+        }
+
+        public static string GetStudentGenderAssist()
+        {
+            //using ( var context = new PersonalSchoolCEntities())
+            //{
+            //  code
+            //  code
+            //  code
+            //  return code;
+            //}
+
+            return "н";
+        }
+
+        public static DateTime GetStudentDateOfBirth(int studentID)
+        {
+            try
+            {
+                using (var context = new PersonalSchoolCardEntities())
+                {
+                    var StudentBirthDate = context.Students
+                        .Where(student => student.StudentID == studentID)
+                        .Select(student => student.DateOfBirth)
+                        .FirstOrDefault()
+                        ;
+
+                    return StudentBirthDate;
+                }
+            }
+            catch (Exception ex)
+            {
+                return DateTime.Now; //need to be checked
+            }
+        }
+
+        public static string GetStudentCurrentAddress(int studentID)
+        {
+            using (var context = new PersonalSchoolCardEntities())
+            {
+                var CurrentAddress = (from s in context.Students
+                                      where s.StudentID == studentID
+                                      select s.Address)
+                                      .FirstOrDefault()
+                                      ;
+
+                return CurrentAddress;
+            }
+        }
+
+        public static string GetStudentMobilePhone(int studentID)
+        {
+            using (var context = new PersonalSchoolCardEntities())
+            {
+                var StudentMobilePhone = context.Students
+                    .Where(student => student.StudentID == studentID)
+                    .Select(student => student.Phone)
+                    .FirstOrDefault()
+                    ;
+
+                return StudentMobilePhone;
+            }
+        }
+
+        public static int GetStudentEnrollmentYear(int studentID)
+        {
+            using (var context = new PersonalSchoolCardEntities())
+            {
+                var StudentEnrollmentYear = context.Students
+                    .Where(student => student.StudentID == studentID)
+                    .Select(student => student.EnrollmentYear)
+                    .FirstOrDefault()
+                    ;
+
+                return StudentEnrollmentYear;
+            }
+        }
+
+    }
+}
+
