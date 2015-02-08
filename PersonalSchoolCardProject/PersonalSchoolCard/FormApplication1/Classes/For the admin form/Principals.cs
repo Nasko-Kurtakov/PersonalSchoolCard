926cfc89f451ae51fc2df6e2ae3c1ce494ccc2d0
Nasko-Kurtakov <nasko.kurtakov@dir.bg>
2015-02-08 20:48:04 +0200

    try

---
+++ PersonalSchoolCardProject/PersonalSchoolCard/FormApplication1/Classes/For the admin form/Principals.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonalShcoolCard.Classes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+    using PersonalSchoolCard.Data;
+
+    public class Principals
+    {
+        public static void NewPrincipal(TextBox firstName, TextBox secondName, TextBox lastName)
+        {
+            using(var contex = new PersonalSchoolCardEntities())
+            {
+                try
+                {
+                    var principal = new Principal
+                    {
+                        FirstName = firstName.Text,
+                        SecondName = secondName.Text,
+                        LastName = lastName.Text
+                    };
+                    contex.Principals.Add(principal);
+                    contex.SaveChanges();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
